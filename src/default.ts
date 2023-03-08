6da3972b5838887e77fbefeb936f9bd2fed23932
蒲月十三 <53714459+maycope@users.noreply.github.com>
2023-03-08 15:01:58 +0800

    1
    1

--- src/default.ts
+++ src/default.ts
@@ -11,6 +11,6 @@ export const defaultMessage = `
 - 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 - 本站由 [黛色翩翩](https://mp.weixin.qq.com/s/HTYv9FxUAJDzh0yWLFNotQ) 搭建。
 
-— 想要部署的朋友，可关注公众号：**黛色翩翩**，添加我个人微信获取保姆级部署文档，
+- 想要部署的朋友，可关注公众号：**黛色翩翩**，添加我个人微信获取保姆级部署文档，
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
