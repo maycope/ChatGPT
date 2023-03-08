bc0c29cc69ab1c60cf227923dece5a59994831bb
蒲月十三 <53714459+maycope@users.noreply.github.com>
2023-03-08 15:11:34 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -9,6 +9,7 @@ export const defaultSetting = {
 export const defaultMessage = `
 
 - 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+
 - 本站由 [黛色翩翩](https://mp.weixin.qq.com/s/HTYv9FxUAJDzh0yWLFNotQ) 搭建。
 
 - 想要部署的朋友，可关注公众号：**黛色翩翩**，添加我个人微信获取保姆级部署文档，
