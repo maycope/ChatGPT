879758c0209549d1bb27cf0b58d4b9979bf0b440
蒲月十三 <53714459+maycope@users.noreply.github.com>
2023-03-09 23:28:52 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -12,6 +12,8 @@ export const defaultMessage = `
 
 - 本站由 [黛色翩翩](https://mp.weixin.qq.com/s/HTYv9FxUAJDzh0yWLFNotQ) 搭建。
 
-- 想要部署的朋友，可关注公众号：**黛色翩翩**，添加我个人微信获取保姆级部署文档，
+- 想要部署的朋友，可关注公众号：**黛色翩翩**，添加我个人微信获取保姆级部署文档。
+
+- 关注公众号：**黛色翩翩**，回复关键字key,获取到最新的AppKey。
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
